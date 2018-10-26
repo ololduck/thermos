992d382610c4b5e4194ebda007c8d859700b267d
Paul Ollivier <contact@paulollivier.fr>
2018-10-26 23:11:18 +0200

    Add logging

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 import glob
+import logging
 import time
 from argparse import ArgumentParser
 
 import RPi.GPIO as GPIO
 
-RELAY_PIN=17
+RELAY_PIN = 17
 
+logging.basicConfig()
 
 # stolen from:
 #  https://cdn-learn.adafruit.com/downloads/pdf/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing.pdf
 # We don't need to modprobe, that should be the os's job
 # os.system('modprobe w1-gpio')
 # os.system('modprobe w1-therm')
+
 base_dir = '/sys/bus/w1/devices/'
 devices = glob.glob(base_dir + '28*')
 if len(devices) < 1:
-    print("there doesn't seem to be a temperature probe. Are the modules loaded?")
+    logging.error("there doesn't seem to be a temperature probe. Are the modules loaded?")
     exit(1)
 device_folder = devices[0]
 device_file = device_folder + '/w1_slave'
@@ -43,39 +46,40 @@ def read_temp():
 
 
 def update_heating_status(heating):
-    print("Setting {} to {}".format(RELAY_PIN, heating))
+    logging.debug("Setting heating to {}".format(heating))
     # The relay is active low....
     GPIO.output(RELAY_PIN, not heating)
 
 
 def main():
-    print("in Main")
     parser.add_argument("-t", "--target-temp", type=float, required=True, help="Target temperature")
-    parser.add_argument("--threshold", type=float, default=1.0,
+    parser.add_argument("--threshold", type=float, default=0.5,
                         help="how many C° should we allow from the target temperature")
     args = vars(parser.parse_args())
-    print(args)
+    logging.debug(args)
 
     try:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(RELAY_PIN, GPIO.OUT)
         GPIO.output(RELAY_PIN, GPIO.LOW)
         heating = False
+        logging.info("Initialized thermostatd with pin %d as actuator and %f C° as target temperature", RELAY_PIN,
+                     args['target_temp'])
         while True:
             temp = read_temp()
-            print("Current temp is %d", temp)
-            if temp < args['target_temp']:
+            logging.debug("Current temp is %d", temp)
+            if temp < args['target_temp'] - args['threshold']:
                 heating = True
             elif temp > args['target_temp'] + args["threshold"]:
                 heating = False
-
             update_heating_status(heating)
-
             time.sleep(10.0)
+
     except KeyboardInterrupt:
+        logging.info("Program interrupted by user.")
+    finally:
         GPIO.output(RELAY_PIN, False)
         GPIO.cleanup()
-        print("Program interrupted by user.")
 
 
 if __name__ == "__main__":
