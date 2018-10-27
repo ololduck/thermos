ebfad16405671685b57e7ebfd83cfc108b5bf936
Paul Ollivier <contact@paulollivier.fr>
2018-10-27 18:59:37 +0200

    Add possibility to persist config object on disk

--- thermos/config.py
+++ thermos/config.py
@@ -1,13 +1,28 @@
 from datetime import datetime
+from logging import getLogger
 
 import toml
+
+logger = getLogger()
 
 
 class Config(dict):
     @staticmethod
     def load(config_file="thermos.toml"):
         with open(config_file) as f:
-            return toml.load(f, _dict=Config)
+            config = toml.load(f, _dict=Config)
+        config.config_file = config_file
+        return config
+
+    def __init__(self, *args, **kwargs):
+        self.config_file = None
+        super(Config, self).__init__(*args, **kwargs)
+
+    def save(self):
+        if self.config_file is None:
+            logger.exception("self.config_file is not defined! that should definitely not happen.")
+        with open(self.config_file, "w+") as f:
+            toml.dump(self, f)
 
     def get_current_scheduled_temperature(self) -> float:
         now = datetime.now()
