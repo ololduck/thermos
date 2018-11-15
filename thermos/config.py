8e4079da306e49574471a978a90dcfca2bc58a58
Paul Ollivier <contact@paulollivier.fr>
2018-11-15 10:03:48 +0100

    Add InfluxDB logging

--- thermos/config.py
+++ thermos/config.py
@@ -1,7 +1,6 @@
+import toml
 from datetime import datetime
 from logging import getLogger
-
-import toml
 
 logger = getLogger()
 
@@ -19,12 +18,14 @@ class Config(dict):
         super(Config, self).__init__(*args, **kwargs)
 
     def save(self):
+        """Persists the configuration on disk"""
         if self.config_file is None:
             logger.exception("self.config_file is not defined! that should definitely not happen.")
         with open(self.config_file, "w+") as f:
             toml.dump(self, f)
 
     def get_current_scheduled_temperature(self):
+        """Returns the current (according to datetime.now()) temperature we should have"""
         now = datetime.now()
         hour = now.hour
         minutes = now.minute
@@ -33,3 +34,25 @@ class Config(dict):
         else:
             minutes = 0
         return self["schedule"]["hourly"]["{:0>2d}:{:0>2d}".format(hour, minutes)]
+
+    def get(self, k, default=None):
+        """
+        returns the value specified by :param k:, where k uses dot-syntax to access fields. See examples
+        :param k: key
+        :param default: a default value to return if nothing is found
+
+
+        >>> d = Config()
+        >>> d["influx"] = {"host": "localhost", "port": 8081}
+        >>> d.get("influx.host")
+        'localhost'
+        >>> d.get("does.not.exist", None)
+        >>> d.get("influx")
+        {'host': 'localhost', 'port': 8081}
+        """
+        if '.' in k:
+            v = self.get(k.split('.')[0], default)
+            if v == default:
+                return default
+            return v.get(".".join(k.split('.')[1:])) or default
+        return super(Config, self).get(k, default) or default
--- thermos/core.py
+++ thermos/core.py
@@ -1,18 +1,31 @@
-import logging
 import time
-from glob import glob
-from signal import signal, SIGINT, SIGTERM
 
+import logging
 from RPi import GPIO
+from datetime import datetime
+from glob import glob
+from influxdb import InfluxDBClient
+from signal import SIGINT, SIGTERM, signal
+
+from thermos import Config
 
 logger = logging.getLogger()
 
 
 class ThermosActuator(object):
-    def __init__(self, config):
+    """Manipulates the relay according to recorded temperatures"""
+
+    def __init__(self, config: Config):
+        """
+        Args:
+            config: an instance of config.Config
+        """
         self.relay_pin = int(config['relay_pin'])
         self.config = config
         self.should_run = True
+        self.client = InfluxDBClient(host=config.get("influxdb.host", 'localhost'),
+                                     port=config.get('influxdb.port', 8086),
+                                     database="thermos")
         base_dir = '/sys/bus/w1/devices/'
         devices = glob(base_dir + '28*')
         if len(devices) < 1:
@@ -21,17 +34,24 @@ class ThermosActuator(object):
         device_folder = devices[0]
         self.device_file = device_folder + '/w1_slave'
         self.init_gpios()
-        self.update_heating_status(False)
+        self._update_heating_status(False)
         signal(SIGINT, self.cleanup)
         signal(SIGTERM, self.cleanup)
         logger.info("Initialized thermostatd with pin %d as actuator", self.config["relay_pin"])
 
-    def update_heating_status(self, heating):
+    def _update_heating_status(self, heating: bool):
+        """
+        Sets the relay pin according to the boolean heating
+        """
         logger.debug("Setting heating to %s", heating)
         # The relay is active low....
         GPIO.output(self.relay_pin, not heating)
 
-    def read_temp(self):
+    def _read_temp(self):
+        """
+        returns the current temperature as measured by the probe, in C°
+        """
+
         def read_temp_raw(device_file):
             f = open(device_file, 'r')
             _lines = f.readlines()
@@ -49,17 +69,29 @@ class ThermosActuator(object):
             return temp_c
         logger.exception("could not read temperature from the probe! could not find the string 't='.")
 
+    def _update_influx(self, temp, measurement='actual'):
+        data = {
+            'measurement': measurement,
+            'time': datetime.now(),
+            'temperature': temp
+        }
+        logger.debug('sending to influx the following data: %s', data)
+        self.client.write(data)
+
     def run(self):
+        """Launches the mainloop"""
         try:
             while self.should_run:
                 scheduled_temp = self.config.get_current_scheduled_temperature()
-                temp = self.read_temp()
+                temp = self._read_temp()
                 logger.debug("scheduled/current temp are %.2f/%.2f", scheduled_temp, temp)
+                self._update_influx(temp)
+                self._update_influx(scheduled_temp, 'scheduled')
                 if temp < scheduled_temp - self.config['threshold']:
                     heating = True
                 elif temp > scheduled_temp + self.config["threshold"]:
                     heating = False
-                self.update_heating_status(heating)
+                self._update_heating_status(heating)
                 time.sleep(10.0)
         finally:
             self.cleanup()
@@ -70,5 +102,8 @@ class ThermosActuator(object):
 
     # Graceful exit
     def cleanup(self, signum=None, frame=None):
+        """
+        Provides a way to cleanly exit the main loop
+        """
         self.should_run = False
         GPIO.cleanup()
